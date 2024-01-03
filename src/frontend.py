7f4a71b043836661b15dc9684cd20513466a5a63
BB <ben@breakpointingbad.com>
2024-01-03 14:08:23 -0700

    Adding front end for web scraper.

---
+++ src/frontend.py
@@ -0,0 +1,17 @@
+
+
+def get_whois():
+    """
+    Get's whois information about the VPN
+    """
+
+def get_google_play():
+    """
+    Gets google play information.
+    """
+
+def main():
+    """ """
+
+if __name__ == '__main__':
+    main()
