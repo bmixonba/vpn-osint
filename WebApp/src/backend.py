ddf0fd67e044d671834162ad4c7b624ad75f843a
Ben <bmixonba@asu.edu>
2025-02-08 17:02:21 -0700

    Upated front-end/back-end 1. Front-end - fixed CRUD for adding and deleteing business information. 2. Backend - added API endpoints to support adding business information.

--- WebApp/src/backend.py
+++ WebApp/src/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
 app = Flask(__name__)
     
 import sys
@@ -11,7 +11,7 @@ def get_db_connection():
     conn = psycopg2.connect(
         dbname="vpnosint_db",
         user="vpnosint_user",
-        password="<PASSWORD>",
+        password="labtrash337",
         host="localhost"
     )
     return conn
@@ -23,21 +23,47 @@ def analyst():
     """ """
     return render_template("analyst.html")
 
-@app.route('/submit_business', methods=['POST'])
+@app.route('/api/submit_business', methods=['POST'])
 def submit_business():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
+    """
+    Create a new business
+
+    database schema for table. 
+    vpnosint_db=# d vpnosint_business_db
+                                                Table "public.vpnosint_business_db"
+           Column       |            Type             | Collation | Nullable |                     Default                      
+    --------------------+-----------------------------+-----------+----------+--------------------------------------------------
+     id                 | integer                     |           | not null | nextval('vpnosint_business_db_id_seq'::regclass)
+     business_name      | character varying(255)      |           | not null | 
+     address            | text                        |           |          | 
+     latitude           | double precision            |           |          | 
+     longitude          | double precision            |           |          | 
+     incorporation_date | timestamp without time zone |           |          | 
+
+    Api submission from front end.
+    api/submit_business/
+        data={'company_id': '-1', 'company_name': 'Test VPN Company', 'address': 'Some funky address', 'latitude': '123123', 'longitude': '123123', 'incorporation_date': '2025-02-05T16:30'}
+    """
+    data = request.json
+    print(f"RAN - api/submit_business/")
+    business_name = data['company_name']
+    address = data['address']
+    latitude = data['latitude']
+    longitude = data['longitude']
+    incorporation_date = data['incorporation_date']
+    print(f"\business_name={business_name}, address={address}, latitude={latitude}, longitude={longitude}, incorporation_date={incorporation_date}")
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('INSERT INTO user_data (name, email, message) VALUES (%s, %s, %s)',
-                (name, email, message))
+    cur.execute('INSERT INTO vpnosint_business_db (business_name, address, latitude, longitude, incorporation_date) VALUES (%s, %s, %s, %s, %s)',
+                (business_name, address, latitude, longitude, incorporation_date))
     conn.commit()
     cur.close()
     conn.close()
 
-    return redirect(url_for('analyst'))
+    """
+    """
+    return jsonify({'ok' : True, "statusText" : "Company Successfully Added."}) 
 
 @app.route('/submit_social_media', methods=['POST'])
 def submit_social_media():
@@ -89,33 +115,39 @@ def vpn_company_webpages():
 @app.route('/api/vpn-company/<int:company_id>')
 def get_company_data(company_id):
     # Fetch data from your database
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute(f'SELECT latitude,longitude,address,business_name,incorporation_date FROM vpnosint_business_db WHERE id={company_id};')
-    locations = cur.fetchall()
-    business_name = locations[0][3]
-    print(locations)
+    if company_id == -1:
+        print(f"get_company_data - 1 - {company_id}")
+        return jsonify({})
 
-    """
-    id, domain_name, address, latitude, longitude, business_id, registration_date, registrar, registrar_abuse_email, registrar_phone, registrant_name, registrant_organization
+    else:
+        print(f"get_company_data - 2 - {company_id}")
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(f'SELECT latitude,longitude,address,business_name,incorporation_date FROM vpnosint_business_db WHERE id={company_id};')
+        locations = cur.fetchall()
+        business_name = locations[0][3]
+        print(locations)
 
-    """
-    cur.execute(f'SELECT latitude,longitude,address,registration_date,registrar,registrant_name FROM vpnosint_domain_db WHERE business_id={company_id};')
-    domain_info = cur.fetchall()
-    print(domain_info)
-    cur.close()
-    conn.close()
+        """
+        id, domain_name, address, latitude, longitude, business_id, registration_date, registrar, registrar_abuse_email, registrar_phone, registrant_name, registrant_organization
 
-    company_data = {
-        "name": business_name,
-        "locations": [{'lat': loc[0], 'lon': loc[1], "address" : loc[2] } for loc in locations] +\
-                [{'lat': loc[0], 'lon': loc[1], "address" : loc[2] } for loc in domain_info],
-        "timelineEvents": [{"label": "Incorporation", "date": loc[4]} for loc in locations] +\
-                [{"label" : "Domain Registration", "date" : loc[3]} for loc in domain_info],
-        "relationships": [{"from": business_name, "to": "Foo",
-                           "from": business_name, "to": "Bar"} for loc in domain_info]
-    }
-    return jsonify(company_data)
+        """
+        cur.execute(f'SELECT latitude,longitude,address,registration_date,registrar,registrant_name FROM vpnosint_domain_db WHERE business_id={company_id};')
+        domain_info = cur.fetchall()
+        print(domain_info)
+        cur.close()
+        conn.close()
+
+        company_data = {
+            "name": business_name,
+            "locations": [{'lat': loc[0], 'lon': loc[1], "address" : loc[2] } for loc in locations] +\
+                    [{'lat': loc[0], 'lon': loc[1], "address" : loc[2] } for loc in domain_info],
+            "timelineEvents": [{"label": "Incorporation", "date": loc[4]} for loc in locations] +\
+                    [{"label" : "Domain Registration", "date" : loc[3]} for loc in domain_info],
+            "relationships": [{"from": business_name, "to": "Foo",
+                               "from": business_name, "to": "Bar"} for loc in domain_info]
+        }
+        return jsonify(company_data)
 
 @app.route('/network-data', methods=['GET'])
 def network_data():
@@ -177,12 +209,12 @@ def index():
 
 @app.route('/example_index')
 def example_index():
-    # conn = get_db_connection()
-    # cur = conn.cursor()
+    conn = get_db_connection()
+    cur = conn.cursor()
 
     # Fetch data for the Form View (e.g., list of companies)
-    # cur.execute('SELECT id, name FROM companies;')
-    companies = ["Company 1","Company 2","Company 3"] # cur.fetchall()
+    cur.execute('SELECT id, business_name FROM vpnosint_business_db;')
+    companies = [[-1, "New"]] + cur.fetchall()
 
     # Fetch data for the Analyst View (e.g., specific company details)
     # company_id = request.args.get('company_id', default=1, type=int)  # Default to first company
@@ -212,20 +244,32 @@ def example_index():
         avg_revenue=avg_revenue
     )
 
-@app.route('/submit_company', methods=['POST'])
-def submit_company():
-    name = request.form['name']
-    revenue = request.form['revenue']
+@app.route('/api/update_business/<int:company_id>', methods=['PUT'])
+def update_business(company_id):
+    """Update existing company information.
 
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('INSERT INTO companies (name, revenue) VALUES (%s, %s)',
-                (name, revenue))
-    conn.commit()
-    cur.close()
-    conn.close()
+    """
+    #  conn = get_db_connection()
+    # cur = conn.cursor()
+    print(f"RAN - api/update_company/{company_id}")
+    return redirect(url_for('example_index'))
 
-    return redirect(url_for('index'))
+@app.route('/api/delete_company/<int:company_id>', methods=['DELETE'])
+def delete_company(company_id):
+    if company_id == -1:
+        return jsonify({'status' : 200, "statusText" : "Nothing to delete."})
+    else:
+        print(f"/api/delete_company/{company_id}={type(company_id)}")
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM vpnosint_business_db WHERE id = %s;',(company_id,));
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        print(f"api/delete_company/{company_id}")
+        return jsonify({'ok' : True, "statusText" : "Company Successfully Deleted."}) 
+
 
 
 def main():
